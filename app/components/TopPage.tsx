088cd0b4c2036b88f105a0617c20b6bf8522f759
Alex Lecusay <alex.lecusay@gmail.com>
2022-02-06 15:55:45 -0500

    Finished preperations for Sparkdev begining.

--- app/components/TopPage.tsx
+++ app/components/TopPage.tsx
@@ -2,7 +2,8 @@ export default function TopPage() {
     return (
       <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
         <h1 className="remix">Welcome to Remix</h1>
-        
+        <h1 className="remix2">SparkDev CheckIn 2020</h1>
+
       </div>
     );
   }
